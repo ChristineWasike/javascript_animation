927570d014c5d90288c9c62895f7c271691776da
ChristineWasike <christinewasike22@gmail.com>
2020-09-12 17:30:05 +0300

    New changes added here

--- story.js
+++ story.js
@@ -4,5 +4,29 @@ let villain = document.querySelector('#villain');
 let lightning = document.querySelector('#lightning');
 
 // Setup animation code here
+let lightningStart = {"left":"290px"};
+let lightningEnd  = {"left": "900px"};
+let options = {"duration": 1000};
+let lightningMiss = {"left": "900px", "top": "10px"};
+
+
+let hitVillian = () =>{
+    let villianStart = {
+        transform: 'rotate(0deg)',
+        opacity: 100
+    };
+
+    let villianEnd = {
+        transform: 'rotate(1000deg)',
+        opacity: 0
+    };
+
+    let options = {"duration": 1500};
+
+    villain.animate([villianStart, villianEnd], options);
+}
 
 // Run animation code here
+lightning.animate([lightningStart, lightningEnd], options).onfinish  = hitVillian;
+// lightning.animate([lightningEnd, lightningStart], options)
+// lightning.animate([lightningStart, lightningMiss], options)
